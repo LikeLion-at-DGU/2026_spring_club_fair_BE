a519f4ce7b47d5eb056ca9124a301cf8b5f3c313
yumiykim <ymkim030403@gmail.com>
2026-02-21 16:11:02 +0900

    ✨ Feat: main 모델 생성 및 admin 초기 세팅 (#1)

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Schedule)
+admin.site.register(Location)
+admin.site.register(Division)
+admin.site.register(Booth)
+admin.site.register(BoothImage)
+admin.site.register(TimeTable)
